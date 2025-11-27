90898f4d15b30b017e0ba29c9a2070aa1a864772
AquaGuardMX <aquaguardmx@gmail.com>
2025-11-26 19:13:54 -0600

    Instalar adaptador de Vercel

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -2,9 +2,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server' // <--- CAMBIA A 'server';
-  
+
+  // <--- CAMBIA A 'server';
+  output: 'server',
+
+  adapter: vercel()
 });
